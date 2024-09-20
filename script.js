5acfefddc826fbc06894888f7666cadb9048626b
Sujata255 <das19sujata@gmail.com>
2024-09-20 20:39:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function getFormvalue() {
-    //Write your code here
+function getFormValue() {
+  // Get the first and last name elements
+  const firstNameInput = document.getElementById("firstName");
+  const lastNameInput = document.getElementById("lastName");
 
+  // Get the values   
+ from the input fields
+  const firstName = firstNameInput.value;
+  const lastName = lastNameInput.value;
+
+  // Print the full name in an alert
+  alert(firstName + " " + lastName);
 }
